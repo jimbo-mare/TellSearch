2f0d7fe3818079f9df67c1a2f1aed98a61c12b15
jimbo-mare <kaasaharu@example.com>
2021-05-06 10:45:07 +0900

    test

--- TellSearch/ViewController.swift
+++ TellSearch/ViewController.swift
@@ -1,19 +1,53 @@
-//
-//  ViewController.swift
-//  TellSearch
-//
-//  Created by Ikumi Jimbo on 2021/04/28.
-//
-
 import UIKit
+import Vision
+import VisionKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupVision()
     }
-
-
+    
+    //以下でURLの取得ができた
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //ファイルを取得するためのアラート
+        let alertController = UIAlertController(title: "確認", message: "選択してください", preferredStyle: .actionSheet)
+        //ファイルアプリを起動するための定義
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "フォトライブラリー", style: .default, handler: { (action) in
+                let imagePickerController = UIImagePickerController()
+                imagePickerController.sourceType = .photoLibrary
+                imagePickerController.delegate = self
+                self.present(imagePickerController, animated: true, completion: nil)
+            })
+            alertController.addAction(photoLibraryAction)
+        }
+    }
+    
+    // Vision requests to be performed on each page of the scanned document.
+    var requests = [VNRequest]()
+    var resultingText = ""
+    
+    // Setup Vision request as the request can be reused
+    func setupVision() {
+        let textRecognitionRequest = VNRecognizeTextRequest { request, _ in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                print("The observations are of an unexpected type.")
+                return
+            }
+            // 解析結果の文字列を連結する
+            let maximumCandidates = 1
+            for observation in observations {
+                guard let candidate = observation.topCandidates(maximumCandidates).first else { continue }
+                
+                self.resultingText += candidate.string + "\n"
+            }
+        }
+        // 文字認識のレベルを設定
+        textRecognitionRequest.recognitionLevel = .accurate
+        self.requests = [textRecognitionRequest]
+    }
 }
 
